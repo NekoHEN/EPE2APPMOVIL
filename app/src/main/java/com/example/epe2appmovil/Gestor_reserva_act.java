99a120e636e2519d97b6abc9c2349fe1553ce973
NekoHEN <nicolas.hen.aran@gmail.com>
2023-05-16 16:29:05 -0400

    Se agregarón las diferentes pantallas

---
+++ app/src/main/java/com/example/epe2appmovil/Gestor_reserva_act.java
@@ -0,0 +1,14 @@
+package com.example.epe2appmovil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Gestor_reserva_act extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestor_reserva);
+    }
+}
---
+++ app/src/main/java/com/example/epe2appmovil/Home_act.java
@@ -0,0 +1,14 @@
+package com.example.epe2appmovil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home_act extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
