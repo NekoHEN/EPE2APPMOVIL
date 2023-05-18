623dee411f1fa3b3478df4b100d933ed19350047
Sebastian <sebitaelgrande13@hotmail.com>
2023-05-17 22:50:28 -0400

    Se agrego Boton para devolver

--- app/src/main/java/com/example/epe2appmovil/Info_act.java
+++ app/src/main/java/com/example/epe2appmovil/Info_act.java
@@ -1,14 +1,29 @@
 package com.example.epe2appmovil;
 
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class Info_act extends AppCompatActivity {
 
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Info_act.this,Home_act.class);
+                startActivity(intent);
+            }
+        });
     }
 }
