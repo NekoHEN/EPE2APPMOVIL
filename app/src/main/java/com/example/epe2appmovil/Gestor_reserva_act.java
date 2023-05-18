d0c9a7ac8678aaeca247f91dc1cf5e2c5814752d
diego-aravena <diegojehovaaravena@gmail.com>
2023-05-17 22:44:08 -0400

    se agrego las base de datos mas 3 images que se usaran para el slider

--- app/src/main/java/com/example/epe2appmovil/Gestor_reserva_act.java
+++ app/src/main/java/com/example/epe2appmovil/Gestor_reserva_act.java
@@ -3,12 +3,32 @@ package com.example.epe2appmovil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 public class Gestor_reserva_act extends AppCompatActivity {
+
+    private ViewFlipper vf;
+    private int[] images = {R.drawable.juego1,R.drawable.juego2,R.drawable.juego3};
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gestor_reserva);
+    }
+
+    public void flipImage(int i)
+    {
+        ImageView view = new ImageView(this);
+        view.setBackgroundResource(i);
+
+        vf.addView(view);
+        vf.setFlipInterval(3000);
+        vf.setAutoStart(true);
+
+        vf.setInAnimation(this, android.R.anim.slide_in_left);
+        vf.setInAnimation(this, android.R.anim.slide_out_right);
     }
 }
